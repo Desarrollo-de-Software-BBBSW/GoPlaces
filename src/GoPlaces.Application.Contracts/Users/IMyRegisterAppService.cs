517d8d33c365611a2ececad5b00f4c6345a5e087
MaxiBlanc <maxiblanc240801@gmail.com>
2025-12-30 10:53:43 -0300

    registro usuario back

---
+++ src/GoPlaces.Application.Contracts/Users/IMyRegisterAppService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace GoPlaces.Users;
+
+public interface IMyRegisterAppService : IApplicationService
+{
+    Task RegisterAsync(RegisterInputDto input);
+}
---
+++ src/GoPlaces.Application/Users/RegisterAppService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Volo.Abp.Identity;
+
+namespace GoPlaces.Users
+{
+    public class MyRegisterAppService : GoPlacesAppService
+    {
+        private readonly IdentityUserManager _userManager;
+
+        public MyRegisterAppService(IdentityUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task RegisterCustomUserAsync(string userName, string email, string password)
+        {
+            // Creamos la entidad del usuario
+            var user = new Volo.Abp.Identity.IdentityUser(
+                GuidGenerator.Create(),
+                userName,
+                email,
+                CurrentTenant.Id
+            );
+
+            // Usamos el manager de ABP para crearlo con password (esto ya lo hashea)
+            (await _userManager.CreateAsync(user, password)).CheckErrors();
+        }
+    }
+}
