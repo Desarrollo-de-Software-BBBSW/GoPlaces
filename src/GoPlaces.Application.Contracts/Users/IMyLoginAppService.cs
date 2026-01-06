af9b0e3f5d613144923ee7d56a6ee61697cd2430
LucasBre55 <lucasbrejoaco@gmail.com>
2026-01-06 18:24:52 -0300

    login usuario back

---
+++ src/GoPlaces.Application.Contracts/Users/IMyLoginAppService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace GoPlaces.Users
+{
+    public interface IMyLoginAppService : IApplicationService
+    {
+        // Devolvemos true si el login es exitoso, o lanzamos excepción si falla
+        Task<bool> LoginAsync(LoginInputDto input);
+    }
+}
