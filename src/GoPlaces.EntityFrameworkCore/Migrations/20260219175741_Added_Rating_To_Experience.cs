6c4fc58b7b38745e92d20b9f231b08945618005a
Thiago Weber <thiagoweber1314@gmail.com>
2026-02-19 15:16:40 -0300

    Filtrar experiencias por valoración-(positiva/negativa/neutral)-(back)

---
+++ src/GoPlaces.EntityFrameworkCore/Migrations/20260219175741_Added_Rating_To_Experience.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoPlaces.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Rating_To_Experience : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "AppExperiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "AppExperiences");
+        }
+    }
+}
